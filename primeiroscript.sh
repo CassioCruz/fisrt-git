f2054e273250c147b32fc91f62fa33c01e3a11e0
CassioCruz <cassiosousa1997@gmail.com>
2020-04-06 22:48:58 +0100

    second stage

--- primeiroscript.sh
+++ primeiroscript.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
 
-echo "Ola seja BEM VINDO"
+echo "Hi welcome first :"
   read nome
-MEDICO1=Cassio
-MEDICO2=Vera
-MEDICO3=Cruz
-echo "Qual turno o $nome deseja consultar"
+consul(){
+echo "Qual turno o $nome deseja consultar:"
    read CONSULTA
 	if [ $CONSULTA = manha ]
  	   then
-	       echo " O Médico $MEDICO1 é  unico a atender no turno $consulta"
-	       echo "a colsulta está ,marcada"
+	       echo " O Médico Vera é unico a atender no turno $consulta."
+	       echo "a consulta está ,marcada. Sr.$nome."
 fi
-sleep 2
-
-#sleep 4
-if [ $CONSULTA = tarde ] 
+sleep 0.5
+if [ $CONSULTA = "tarde" ] 
    then
-      echo " O Médico $MEDICO2 é  unico a atender no turno $consulta"
-      echo "a colsulta está ,marcada"
+      echo "The Doctor Vera is only atender no turno $consulta"
+      echo "a colsulta está ,marcada\n"
 fi
 
-	if [ $CONSULTA = noite ]
+	if [ $CONSULTA = "noite" ]
  	   then
-	       echo " O Médico $MEDICO3 é  unico a atender no turno $consulta"
+	       echo " The Doctor Cruz é unico a atender no turno $consulta"
 	       echo "A consulta está ,marcada obrigado"
+	fi
+}
+consul
+echo "Deseja voltar a consulta Sr $nome"
+   read back
+if [ $back = "yes" ]
+   then
+	consul
+else
+   echo "Have a good day"
 fi
-
-
 
